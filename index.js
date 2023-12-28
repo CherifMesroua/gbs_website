e3c42b79cbe897008335009d9bf7346487787ccf
CherifMesroua <mes.cherife@gmail.com>
2023-12-28 18:16:29 +0000

    commit 2

---
+++ index.js
@@ -0,0 +1,6 @@
+const inputfiled= document.getElementById('inputfiled');
+const addbtn= document.getElementById('addbtn');
+
+addbtn.addEventListener('click', function(){
+    console.log(inputfiled.value);
+});
