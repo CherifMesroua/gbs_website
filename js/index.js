ea98ca26525c08e63dd9a67a784d45c1a0768abe
CherifMesroua <mes.cherife@gmail.com>
2023-12-31 00:03:49 +0000

    in the way to send the data and create a firebase db according to the excel file

--- js/index.js
+++ js/index.js
@@ -2,11 +2,23 @@ import {getSelectedRadioValue} from "./fonctions.js";
 import { initializeApp  } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getDatabase, ref, set,push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
+const appsettings = {
+    databaseURL: "https://gestiones-ddc58-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appsettings);
+const database = getDatabase();
+const numRows = 60;
+const numCols = 10;
+
 document.addEventListener('DOMContentLoaded', function(){
 const ajouterunbon = document.getElementById('ajouterunbon');
 const afficherexcel= document.getElementById('afficherexcel');
 const input = document.getElementById('inputfile');
 const displaytext = document.getElementById('displaytext');
+const envoiyerversleserveur = document.getElementById('envoiyerversleserveur');
+let dbconstracter=[];
+
 
 afficherexcel.addEventListener('click', function () {
     if (!input.files || input.files.length === 0) {
@@ -18,7 +30,7 @@ afficherexcel.addEventListener('click', function () {
 
     readXlsxFile(file).then(function (data) {
         let arrayData = data;
-        console.log(arrayData.length);
+
         let dataMatrix= [];
         for (let i = 0; i < 60; i++) {
             dataMatrix[i] = [];
@@ -26,6 +38,8 @@ afficherexcel.addEventListener('click', function () {
                 dataMatrix[i][j]=arrayData[i][j];
             }
         }
+
+        // ===========================this code show the result in tetxarea =============================
         // const numRows = 60;
         // const numCols = 10;
 
@@ -47,7 +61,11 @@ afficherexcel.addEventListener('click', function () {
         }
 
         displaytext.innerHTML = JSON.stringify(jsonData, null, 2);
-        console.log(typeof jsonData);
+        dbconstracter = JSON.stringify(jsonData, null, 2);
+        dbconstracter = JSON.parse(dbconstracter);
+        console.log(Array.isArray(dbconstracter));
+
+
     }).catch(function (error) {
         console.error("Error reading the file:", error);
     });
@@ -57,23 +75,46 @@ ajouterunbon.addEventListener('click', function(){
     const matricule = document.getElementById('Matricule');let matriculevalue=matricule.value;
     const nomlabel= document.getElementById('nomlabel');
     const matriculelabel= document.getElementById('matriculelabel');
-
-    const appsettings = {
-        databaseURL: "https://gestiones-ddc58-default-rtdb.europe-west1.firebasedatabase.app/"
-    }
-
-    const app = initializeApp(appsettings);
-    const database = getDatabase();
     const dbRef = ref(database, matriculevalue);
-
+    
     var selectedValue = getSelectedRadioValue('input[name="bonType"]:checked');
     const currentDate = new Date();
 
     console.log("nom: ",nomvalue,"type: ",selectedValue,"date: ",Date(currentDate.toString()));
     matriculelabel.innerHTML +=  `${matriculevalue}`;
     nomlabel.innerHTML += `${nomvalue}`;
-    // push(dbRef,{"nom":nomvalue,"bonType":selectedValue,"date":currentDate.toString()});
+    push(dbRef,{"nom":nomvalue,"bonType":selectedValue,"date":currentDate.toString()});
 
+});
+
+envoiyerversleserveur.addEventListener('click', function(){
+    
+    for (const item of dbconstracter) {
+        // const dbRef = ref(database, item.Matricule);
+    
+        // set(dbRef, {
+        //     "Nom": item.Nom,
+        //     "Catégorie": item.Catégorie,
+        //     "Regime": item.Regime,
+        //     "Temps1": item.Temps1,
+        //     "Temps2": item.Temps2,
+        //     "Sexe": item.Sexe,
+        //     "Prenom": item.Prenom,
+        //     "Fonction": item.Fonction,
+        //     "Structure": item.Structure
+        // });
+        console.log(
+            "Nom: " + item.Nom,
+            "Catégorie: " + item.Catégorie,
+            "Regime: " + item.Regime,
+            "Temps1: " + item.Temps1,
+            "Temps2: " + item.Temps2,
+            "Sexe: " + item.Sexe,
+            "Prenom: " + item.Prenom,
+            "Fonction: " + item.Fonction,
+            "Structure: " + item.Structure+"\n"
+        );
+      }
 });
 
 });
